a50f13236c2bdbcb1f291db4f83980c4616e8e61
YouJuneYoung <mamwli21@naver.com>
2016-07-09 16:34:33 +0900

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "sum.h"
+#include <iostream> // search first at system folder
+using namespace std;
+int main()
+{
+     int a;
+     cin >> a;
+     cout << sum(a) << endl;
+     return 0;
+}
